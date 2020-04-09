4c9fd9a20574b88d09b399fa67f65f3b0483a325
Will Ryan <will.ryan@gluwa.com>
2020-04-09 16:31:52 -0700

    Add SendFunds endpoint to CCaaS

---
+++ Creditcoin/ccaas/Models/SendFundsQueryParam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ccaas.Models
+{
+    public class SendFundsQueryParam
+    {
+        public string key;
+        public SendFundsQuery query;
+    }
+}
